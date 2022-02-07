c3adac7faa7d931105c327ec067cbe4a0ed3ca5b
Rawan <rawanalex15@gmail.com>
2022-02-07 11:09:33 +0200

    edit employee view

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,7 @@
 class Employee < ApplicationRecord
-    validates :name, presence: true#, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :email, presence: true, format: { with: /\w+@[a-zA-Z0-9]+.com/, message: "not valid email"}
+    validates :job, presence: true
 
     belongs_to :team, optional: true
     belongs_to :division
