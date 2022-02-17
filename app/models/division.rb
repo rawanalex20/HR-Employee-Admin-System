f84b94b1b344b267b68209c8e4bbbc5996eb40d7
Rawan <rawanalex15@gmail.com>
2022-02-17 09:32:25 +0200

    remove some comments in division.rb

--- app/models/division.rb
+++ app/models/division.rb
@@ -2,6 +2,6 @@ class Division < ApplicationRecord
     validates :name, presence: true
     
     has_many :teams, dependent: :destroy
-    has_many :employees, dependent: :nullify#, foreign_key: "employee_id"
-    belongs_to :employee, optional: true, class_name: "Division"#, foreign_key: "manager_id"
+    has_many :employees, dependent: :nullify
+    belongs_to :employee, optional: true, class_name: "Division"
 end
