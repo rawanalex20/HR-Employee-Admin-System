f4d24075bd09b5278ebb917bf5870398e1d642b1
Rawan <rawanalex15@gmail.com>
2022-02-26 03:29:20 +0200

    Destroy targets on deleting team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
     validates :name, presence: true
 
-    has_many :targets, dependent: :nullify
+    has_many :targets, dependent: :destroy
     has_many :employees, dependent: :nullify
     belongs_to :division, class_name: "Division"
     belongs_to :employee, optional: true, class_name: "Team"
