7a8e4324795a6f21d41ed89fcab70ce823138ae2
Rawan <rawanalex15@gmail.com>
2022-02-27 16:05:03 +0200

    User upload profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-    validates :name, :email, :password, :password_confirmation, presence: true
-    validates :name, format: { with: /\A[a-zA-Z_ ]+\z/, message: "only allows letters and spaces" }
+    has_one_attached :profile
+
     validates :email, format: { with: /\w+@[a-zA-Z0-9]+.com/, message: "not valid email"}
-    validates :password, length: { minimum: 4 }
-    validates :password, confirmation: true
 end
