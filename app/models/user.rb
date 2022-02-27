61f4d038e88a94f121f76222675e292479c76135
Rawan <rawanalex15@gmail.com>
2022-02-27 10:45:40 +0200

    Basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    # validates :name, presence: true
-    # validates :name, format: { with: /\A[a-zA-Z_ ]+\z/, message: "only allows letters and spaces" }
-    validates :email, format: { with: /\w+@[a-zA-Z0-9]+.com/, message: "not valid email"}
 end
