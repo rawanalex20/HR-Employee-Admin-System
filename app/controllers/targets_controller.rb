a9f2c75b14e2cfe034dc14a4a8e821325b5c484d
Rawan <rawanalex15@gmail.com>
2022-03-02 16:33:01 +0200

    Create, update and destroy notice translations

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -25,7 +25,7 @@ class TargetsController < ApplicationController
 
     respond_to do |format|
       if @target.save
-        format.html { redirect_to target_url(@target), notice: "Target was successfully created." }
+        format.html { redirect_to target_url(@target), notice: t('.success') }
         format.json { render :show, status: :created, location: @target }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TargetsController < ApplicationController
   def update
     respond_to do |format|
       if @target.update(target_params)
-        format.html { redirect_to target_url(@target), notice: "Target was successfully updated." }
+        format.html { redirect_to target_url(@target), notice: t('.success') }
         format.json { render :show, status: :ok, location: @target }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class TargetsController < ApplicationController
     @target.destroy
 
     respond_to do |format|
-      format.html { redirect_to targets_url, notice: "Target was successfully destroyed." }
+      format.html { redirect_to targets_url, notice: t('.success') }
       format.json { head :no_content }
     end
   end
