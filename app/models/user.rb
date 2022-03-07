dcad117b3f7e70cadc7fd2736c731a696dcbe256
Rawan <rawanalex15@gmail.com>
2022-03-07 12:42:08 +0200

    Remove extra validation from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_one_attached :profile
 
-    validates :name, format: { with: /\A[a-zA-Z_ ]+\z/, message: "only allows letters and spaces" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
