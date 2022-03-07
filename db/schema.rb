c39c572c4cf1bcf1f7b49d64cc4392fce21c883d
Rawan <rawanalex15@gmail.com>
2022-03-07 09:04:46 +0200

    New columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_080428) do
+ActiveRecord::Schema.define(version: 2022_03_07_065453) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: 6, null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "divisions", force: :cascade do |t|
     t.string "name"
@@ -57,13 +85,22 @@ ActiveRecord::Schema.define(version: 2022_02_26_080428) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "name"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "remember_created_at", precision: 6
+    t.datetime "reset_password_sent_at", precision: 6
+    t.string "reset_password_token"
+    t.string "encrypted_password", default: "", null: false
+    t.string "email"
+    t.string "password"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "last_sign_in_count", precision: 6
+    t.datetime "current_sign_in_count", precision: 6
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "divisions", "employees", column: "manager_id"
   add_foreign_key "employees", "teams"
   add_foreign_key "targets", "teams"
