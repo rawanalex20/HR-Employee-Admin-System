b5fa3617778d582dccdf95f47f903d901c950239
Rawan <rawanalex15@gmail.com>
2022-03-24 22:51:10 +0200

    Remove extra columns from fixures and add required gem for a win platform

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #fixtures :employees
+  test "save employee" do
+    employee = Employee.new(name: "employee 1")
+    # employee.email = employees(:one).email
+    # employee.name = employees(:one).name
+    # employee.job = employees(:one).job
+    # employee.salary = employees(:one).salary
+    # employee.employment_status = employees(:one).employment_status
+
+    assert_not employee.save
+  end
+
 end
