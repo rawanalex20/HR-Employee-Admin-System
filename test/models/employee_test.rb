dc7abe95cbd69ef2a2c33a6ed28761a1dfe29a94
Rawan <rawanalex15@gmail.com>
2022-03-25 11:12:21 +0200

    model tests

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -2,16 +2,64 @@ require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
 
-  #fixtures :employees
+  fixtures :employees
   test "save employee" do
-    employee = Employee.new(name: "employee 1")
-    # employee.email = employees(:one).email
-    # employee.name = employees(:one).name
-    # employee.job = employees(:one).job
-    # employee.salary = employees(:one).salary
-    # employee.employment_status = employees(:one).employment_status
+    employee = Employee.new
+    employee.email = employees(:one).email
+    employee.name = employees(:one).name
+    employee.job = employees(:one).job
+    employee.salary = employees(:one).salary
+    employee.employment_status = employees(:one).employment_status
+
+    team = Team.create(name: "team1")
+    employee.team_id = team.id
+
+    assert employee.save
+  end
+
+  test "shouldn't save employee as invalid email" do
+    employee = Employee.new
+    employee.email = "employee"
+    employee.name = employees(:one).name
+    employee.job = employees(:one).job
+    employee.salary = employees(:one).salary
+    employee.employment_status = employees(:one).employment_status
 
     assert_not employee.save
+  end
+
+  test "shouldn't save employee as invalid name" do
+    employee = Employee.new
+    employee.email = employees(:one).email
+    employee.name = "employee123"
+    employee.job = employees(:one).job
+    employee.salary = employees(:one).salary
+    employee.employment_status = employees(:one).employment_status
+
+    assert_not employee.save
+  end
+
+  test "shouldn't save employee because of not null contraint" do
+    employee = Employee.new
+    employee.email = employees(:one).email
+    employee.name = employees(:one).name
+    employee.salary = employees(:one).salary
+    employee.employment_status = employees(:one).employment_status
+
+    assert_not employee.save
+  end
+
+  test "job should be equal job in fixture" do
+    employee = Employee.new
+    employee.email = employees(:two).email
+    employee.name = employees(:two).name
+    employee.job = employees(:two).job
+    employee.salary = employees(:two).salary
+    employee.employment_status = employees(:two).employment_status
+
+    employee.save
+
+    assert_equal(employees(:two).job, Employee.last.job)
   end
 
 end
